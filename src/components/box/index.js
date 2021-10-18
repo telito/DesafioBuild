6c5dff39868d5a4ed7b4c00a765e62922d60b6f7
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-10-18 14:27:47 -0300

    mais alguns ajustes pedidos no build

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Box(props) {
     return (
         <>
-            <div className="card  mb-3 box" >
+            <div className="card  mb-3 box" key={props.key} >
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.text}</p>
