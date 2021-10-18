ddb6b35d387b28318b4f534b6c58248b1c950935
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-10-18 15:20:53 -0300

    ajustando responsividade

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -4,7 +4,7 @@ import {Col} from 'react-bootstrap'
 function Box(props) {
     return (
         <Col className="col-12 col-md-4" key={props.key}>
-            <div className="card  mb-3 box">
+            <div className="card mb-3 box">
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.text}</p>
