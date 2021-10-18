c5c5dfec25d630b1e487dfc0da0445cbf7e3e349
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-10-18 14:35:30 -0300

    Retirando redundancias

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Box(props) {
     return (
-        <>
-            <div className="card  mb-3 box" key={props.key} >
+        
+            <div className="card  mb-3 box" key={props.key}>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.text}</p>
                 </div>
             </div>
-        </>
+        
     )
 }
 
